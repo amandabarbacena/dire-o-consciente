e48526ddb4c98f3db2993522cc24cfad7b3d06b0
Amanda Barbacena <amanda@abpsicologia.com.br>
2026-08-15 23:43:08 +0000

    Troca o container GTM para um dedicado a este site (GTM-PNGBQX4D), separado do site antigo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -117,14 +117,14 @@ function RootShell({ children }: { children: ReactNode }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WHNFR87Z');`,
+})(window,document,'script','dataLayer','GTM-PNGBQX4D');`,
           }}
         />
       </head>
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WHNFR87Z"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PNGBQX4D"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
